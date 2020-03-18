df7a96a7a03686cf53de6dcae558bfcd252cdf73
Oswaldo Andrés <48687178+Oswalman@users.noreply.github.com>
2020-03-18 11:16:52 -0500

    repair bug

--- lib/Views/Login/Login.dart
+++ lib/Views/Login/Login.dart
@@ -7,68 +7,74 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar:
+          Align(alignment: Alignment.bottomCenter, child: Text('AGROMANAGER')),
       body: Column(children: [
-        Image.asset(
-          'assets/Fondo_Login.png',
+        Image(
+          image: AssetImage('assets/Fondo_Login.png'),
           width: 600,
-          height: 240,
+          height: 340,
           fit: BoxFit.cover,
         ),
         Form(
-          key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Row(
-                children: <Widget>[
-                  Icon(Icons.account_box, color: Colors.black),
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      hintText: 'Nombre',
-                    ),
-                    validator: (value) {
-                      if (value.isEmpty) {
-                        return 'Por favor, escribe el nombre';
-                      }
-                      return null;
-                    },
-                  )
-                ],
-              ),
-              Row(
-                children: <Widget>[
-                  Icon(Icons.account_box, color: Colors.black),
-                  TextFormField(
-                    obscureText: true,
-                    decoration: const InputDecoration(
-                      hintText: 'Contraseña',
-                    ),
-                    validator: (value) {
-                      if (value.isEmpty) {
-                        return 'Por favor, escribe la contraseña';
-                      }
-                      return null;
-                    },
-                  )
-                ],
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 16.0),
-                child: RaisedButton(
-                  color: Colors.blue,
-                  onPressed: () {
-                    // Valida si el form es correcto
-                    //
-                    if (_formKey.currentState.validate()) {
-                      // Enviar al backend.
-                    }
-                  },
-                  child: Text('INICIAR SESIÓN'),
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    Expanded(
+                        child: Icon(Icons.account_box, color: Colors.black)),
+                    Expanded(
+                        child: TextFormField(
+                      decoration: const InputDecoration(
+                        hintText: 'Nombre',
+                      ),
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Por favor, escribe el nombre';
+                        }
+                        return null;
+                      },
+                    )),
+                  ],
                 ),
-              ),
-            ],
-          ),
-        )
+                Row(
+                  children: <Widget>[
+                    Expanded(child: Icon(Icons.lock, color: Colors.black)),
+                    Expanded(
+                        child: TextFormField(
+                      obscureText: true,
+                      decoration: const InputDecoration(
+                        hintText: 'Contraseña',
+                      ),
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Por favor, escribe la contraseña';
+                        }
+                        return null;
+                      },
+                    ))
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16.0),
+                  child: RaisedButton(
+                    color: Colors.blue,
+                    textColor: Colors.white,
+                    onPressed: () {
+                      // Valida si el form es correcto
+                      //
+                      if (_formKey.currentState.validate()) {
+                        // Enviar al backend.
+                      }
+                    },
+                    child: Text('INICIAR SESIÓN'),
+                  ),
+                ),
+              ],
+            ))
       ]),
     );
   }
